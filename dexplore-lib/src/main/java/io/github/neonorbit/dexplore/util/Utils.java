9284cb6655db1f868e80e17048eacde5cd91532a
NeonOrbit <acc.neonic@gmail.com>
2022-09-21 13:16:12 +0000

    Fix invalid name

--- dexplore-lib/src/main/java/io/github/neonorbit/dexplore/util/Utils.java
+++ dexplore-lib/src/main/java/io/github/neonorbit/dexplore/util/Utils.java
@@ -86,9 +86,7 @@ public final class Utils {
     for (String s : name.split("\\.", -1)) {
       if (s.isEmpty())
         return false;
-      if (!Character.isJavaIdentifierStart(s.charAt(0)))
-        return false;
-      for (int i = 1; i < s.length(); i++) {
+      for (int i = 0; i < s.length(); i++) {
         if (!Character.isJavaIdentifierPart(s.charAt(i)))
           return false;
       }
